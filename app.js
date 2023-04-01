bbd4800ec622f36ada180a95df9fc23e15b9fbd3
kumarpanu <kapnuganti82@gmail.com>
2023-04-01 08:08:31 +0000

    gadgets page

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("app running at http://localhost:3000/");
+});
+
+app.get("/gadgets", (request, response) => {
+  response.sendFile("./gadgets.html", { root: __dirname });
+});
+
+module.exports = app;
